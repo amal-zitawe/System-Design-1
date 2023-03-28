cc1d6b7fde31deeb4bd39403202a8ebea9b0d6c3
Amal Zetawi <94221592+amal-zitawe@users.noreply.github.com>
2023-03-28 14:16:48 +0300

    login

--- Login.dart
+++ Login.dart
@@ -1,22 +1,23 @@
 import 'dart:convert';
 import 'dart:io';
-// ignore: unused_import
 import 'package:crypt/crypt.dart';
-import 'home.dart';
+import 'Home.dart';
+import 'Welcome.dart';
 
 int count = 0;
 
 class Login {
+  static var loginUser;
   login() async {
-    print("Welcom at Login page");
+    print("Welcom to the Login page");
     print("--------------------------");
     print("Please enter your details ");
     stdout.write("a. Email address:");
     String? email = stdin.readLineSync();
-    if (email!.isEmpty) {
+    if (email != null && email.isEmpty) {
       print("This field is required");
       count++;
-    } else if (email.isNotEmpty) {
+    } else if (email != null && email.isNotEmpty) {
       String pattern = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
       RegExp regExp = RegExp(pattern);
       if (!regExp.hasMatch(email)) {
@@ -25,15 +26,16 @@ class Login {
       }
     }
     if (count == 2) {
-      home g = new home();
+      Home g = new Home();
+      g.start();
     }
     //............................................
     stdout.write("b. Password:");
     String? password = stdin.readLineSync();
-    if (password!.isEmpty) {
+    if (password != null && password.isEmpty) {
       print("This field is required");
       count++;
-    } else if (password.isNotEmpty) {
+    } else if (password !=null && password.isNotEmpty) {
       String pattern = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$';
       RegExp regExp = RegExp(pattern);
       if (!regExp.hasMatch(password)) {
@@ -42,23 +44,21 @@ class Login {
       }
     }
     if (count == 2) {
-      home g = new home();
+      Home g = new Home();
     }
     //.........................................................
-    else if (password.isNotEmpty && email.isNotEmpty) {
-      String path =
-          'C:/Users/s1181/Desktop/trainingASAL/SystemDesign(1)/data.json';
+    else if (password!.isNotEmpty && email!.isNotEmpty) {
+      String path = './data.json';
       if ((await File(path).existsSync())) {
         String fileContents = await File(path).readAsString();
         List d = json.decode(fileContents);
         final pHashing = Crypt.sha256('$password', salt: 'abcdefghijklmnop');
-        // print(pHashing);
         for (int i = 0; i < d.length; i++) {
           if ((d[i]['email'] == '$email') &&
               (d[i]['password'] == '$pHashing')) {
-            //TODO
-            // Welcome w = new Welcome();
-            // w.welcome();
+            loginUser = d[i]['email'];
+            Welcome w = new Welcome();
+            w.welcome();
             break;
           }
         }
