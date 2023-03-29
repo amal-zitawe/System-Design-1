d9a710870aa1ce69efb7c64b3c0fa6e1b107b139
Amal Zetawi <94221592+amal-zitawe@users.noreply.github.com>
2023-03-30 01:51:27 +0300

    SignUp page edit

--- Sign-up.dart
+++ Sign-up.dart
@@ -1,38 +1,48 @@
 import 'dart:io';
 import 'dart:convert';
-import 'Welcome.dart';
-import 'home.dart';
-// ignore: unused_import
 import 'package:crypt/crypt.dart';
+import 'App.dart';
 
-class Sing_up {
+class SignUp {
+  static var loginUser;
   Sing() async {
     int count = 0;
-    print("Welcom at Sing Up page");
+    print("Welcom to Sing Up page");
     print("--------------------------");
     print("Please put your information");
     //..............................................
     stdout.write("a. FullName:");
     String? name = stdin.readLineSync();
-    if (name!.isEmpty) {
+    if (name != null && name.isEmpty) {
       print("This field is required");
-      count++;
+      stdout.write("a. FullName (once again):");
+      String? nameagain = stdin.readLineSync();
+      name = nameagain;
+      if (nameagain != null && nameagain.isEmpty) {
+        print("This field is required");
+        count++;
+      }
     }
     if (count > 1) {
-      home g = new home();
+      App().runSignUp(UserOptions.home);
     }
 //......................................................
-    stdout.write("b. Birthdate (mm/dd/yyyy):");
+    stdout.write("b. Birthdate (YYYY-MM-DD):");
     String? birthdate = stdin.readLineSync();
-    if (birthdate!.isEmpty) {
+    if (birthdate != null && birthdate.isEmpty) {
       print("This field is required");
-      count++;
+      stdout.write("b. Birthdate (once again):");
+      String? birthdateagain = stdin.readLineSync();
+      birthdate = birthdateagain;
+      if (birthdateagain != null && birthdateagain.isEmpty) {
+        print("This field is required");
+        count++;
+      }
     }
     if (count > 1) {
-      home g = new home();
-    } else if (birthdate.isNotEmpty) {
-      String pattern =
-          r'^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d$';
+      App().runSignUp(UserOptions.home);
+    } else if (birthdate != null && birthdate.isNotEmpty) {
+      String pattern = r'^([1-2]\d{3})\-([0-1][1-9])\-([0-3][0-9])$';
       RegExp regExp = RegExp(pattern);
       if (!regExp.hasMatch(birthdate)) {
         print("the birthdate is not valid ");
@@ -42,9 +52,16 @@ class Sing_up {
 //.........................................................
     stdout.write("c. Email address:");
     String? email = stdin.readLineSync();
-    if (email!.isNotEmpty) {
-      String path =
-          'C:/Users/s1181/Desktop/trainingASAL/SystemDesign(1)/data.json';
+    if (email != null && email.isNotEmpty) {
+      String path = './data.json';
+
+      String pattern = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
+      RegExp regExp = RegExp(pattern);
+      if (!regExp.hasMatch(email)) {
+        print("the email is not valid");
+        count++;
+      }
+
       if ((await File(path).existsSync())) {
         String fileContents = await File(path).readAsString();
         List d = json.decode(fileContents);
@@ -57,30 +74,35 @@ class Sing_up {
         }
       }
     }
-    if (email.isEmpty) {
+    if (email != null && email.isEmpty) {
       print("This field is required");
-      count++;
-    }
-    if (count > 1) {
-      home g = new home();
-    } else if (email.isNotEmpty) {
-      String pattern = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
-      RegExp regExp = RegExp(pattern);
-      if (!regExp.hasMatch(email)) {
-        print("the email is not valid");
+      stdout.write("c. email (once again):");
+      String? emailagain = stdin.readLineSync();
+      birthdate = emailagain;
+      if (emailagain != null && emailagain.isEmpty) {
+        print("This field is required");
         count++;
       }
+    }
+    if (count > 1) {
+      App().runSignUp(UserOptions.home);
     }
 //...............................................................
     stdout.write("d. Password:");
     String? password = stdin.readLineSync();
-    if (password!.isEmpty) {
+    if (password != null && password.isEmpty) {
       print("This field is required");
-      count++;
+      stdout.write("d. Password (once again):");
+      String? passagain = stdin.readLineSync();
+      birthdate = passagain;
+      if (passagain != null && passagain.isEmpty) {
+        print("This field is required");
+        count++;
+      }
     }
     if (count > 1) {
-      home g = new home();
-    } else if (password.isNotEmpty) {
+      App().runSignUp(UserOptions.home);
+    } else if (password != null && password.isNotEmpty) {
       String pattern = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$';
       RegExp regExp = RegExp(pattern);
       if (!regExp.hasMatch(password)) {
@@ -89,12 +111,11 @@ class Sing_up {
       }
     }
     //hasing password
-    final pHashing = Crypt.sha256('$password');
+    final pHashing = Crypt.sha256('$password', salt: 'abcdefghijklmnop');
     //....................................................
     // Read the existing data from the file (if any)
     List<Map<String, dynamic>> data = [];
-    String path =
-        'C:/Users/s1181/Desktop/trainingASAL/SystemDesign(1)/data.json';
+    String path = './data.json';
     String fileContents = await File(path).readAsString();
     List<Map<String, dynamic>>? v = (jsonDecode(fileContents) as List)
         .map((e) => e as Map<String, dynamic>)
@@ -104,6 +125,7 @@ class Sing_up {
     Map<String, dynamic> newData = {
       'fullname': '$name',
       'email': '$email',
+      'birthday': '$birthdate',
       'password': '$pHashing',
     };
     // Append the data object to the list
@@ -113,7 +135,8 @@ class Sing_up {
     // Create a new file and write the JSON string to the file
     File file = File('data.json');
     file.writeAsStringSync(jsonString);
+    loginUser = email;
     print('Successfully Registered');
-    Welcome();
+    App().runSignUp(UserOptions.welcome);
   }
 }
